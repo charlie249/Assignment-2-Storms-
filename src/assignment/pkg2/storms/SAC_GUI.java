aebd8ac7ac6ec1bdbc49f797e12b5fee83800363
charlie249 <56076410+charlie249@users.noreply.github.com>
2020-01-08 16:58:35 +0000

    Skeleton SAC_GUI

--- src/assignment/pkg2/storms/SAC_GUI.java
+++ src/assignment/pkg2/storms/SAC_GUI.java
@@ -5,10 +5,44 @@
  */
 package assignment.pkg2.storms;
 
+import javax.swing.JFrame;
+
 /**
  *
  * @author charlie
  */
-public class SAC_GUI {
+public class SAC_GUI extends JFrame {
+    
+    
+    public SAC_GUI(Company SAC)
+    {
+        // layout
+        
+        
+        // add list box
+        
+        
+        // give storms to list box
+        
+        
+        // listen to list item being clicked
+        
+           // show editor for this list item
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        
+        // create company
+        Company SAC = new Company();
+        
+        // look how your sample code createa a new frame
+        SAC_GUI frame = new SAC_GUI(SAC);
+        frame.pack();
+        
+    }
     
 }
