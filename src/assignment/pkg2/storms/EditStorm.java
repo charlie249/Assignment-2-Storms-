92b4da5cffbc1de56917139132114155e304b046
Ian Mayo <ian@planetmayo.com>
2020-01-09 15:35:07 +0000

    Only save on 'save' button.

--- src/assignment/pkg2/storms/EditStorm.java
+++ src/assignment/pkg2/storms/EditStorm.java
@@ -10,10 +10,11 @@ import java.awt.event.ActionListener;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
-import javax.swing.JFrame;
 import javax.swing.JButton;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -21,106 +22,132 @@ import java.awt.GridBagLayout;
  */
 public class EditStorm extends JPanel {
 
-    public JLabel lblTitle;
     public JLabel lblStormName;
     public JLabel lblWindSpeed;
     public JLabel lblTemperature;
+    public JLabel lblWarning;
 
     public JTextField txtStormName;
     public JTextField txtWindSpeed;
     public JTextField txtTemperature;
 
-    public JButton btnEdit;
+    public JButton btnSave;
     private Storm currentStorm;
 
     private GridBagConstraints constraints;
+    private final JButton btnDelete;
 
-    public EditStorm() {
+    public EditStorm(final JList list) {
+
         this.setLayout(new GridBagLayout());
-
-        lblTitle = new JLabel("Edit Storm");
 
         lblStormName = new JLabel("Storm Name: ");
         lblWindSpeed = new JLabel("WindSpeed: ");
         lblTemperature = new JLabel("Temperature: ");
+        lblWarning = new JLabel("---");
 
         txtStormName = new JTextField();
         txtStormName.setColumns(10);
         txtWindSpeed = new JTextField();
+        txtWindSpeed.setColumns(10);
         txtTemperature = new JTextField();
+        txtTemperature.setColumns(10);
 
-        txtStormName.addActionListener(new ActionListener() {
+
+        btnSave = new JButton("Save new storm data");
+        btnSave.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
-                // do we have a storm?
-                if (currentStorm != null) {
-                    // set the storm to the new value
+                if(currentStorm != null)
+                {
                     currentStorm.setName(txtStormName.getText());
-                }
-            }
-        });
-
-        txtWindSpeed.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if(currentStorm != null){
+                    try
+                    {
                     currentStorm.setWindSpeed(Integer.parseInt(txtWindSpeed.getText()));
-                }
-            }
-        });
-        
-        txtTemperature.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e){
-                if(currentStorm != null){
+                    } catch(NumberFormatException nfe)
+                    {
+                        JOptionPane.showMessageDialog(null, "Badly formatted speed, please try again");
+                    }
+                    try
+                    {
                     currentStorm.setTemp(Integer.parseInt(txtTemperature.getText()));
+                    } catch(NumberFormatException nfe)
+                    {
+                        JOptionPane.showMessageDialog(null, "Badly formatted temperature, please try again");
+                        
+                    }
+                    lblWarning.setText("<html>" + currentStorm.getWarning() + "</html>");
+
+                    list.setModel(list.getModel());
+                    list.updateUI();
                 }
             }
         });
-        
-        btnEdit = new JButton("Save new storm data");
 
+        btnDelete = new JButton("Delete storm");
+        btnDelete.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(currentStorm != null)
+                {
+                    currentStorm.setName(null);
+                    currentStorm.setWindSpeed(0);
+                    currentStorm.setTemp(0);
+                    list.setModel(list.getModel());
+                    list.updateUI();
+                }
+            }
+        });
+
+
+        
         constraints = new GridBagConstraints();
-        
-        
-        constraints.gridy = 0; //sets row
-        constraints.gridx = 0; // sets coloumn
-        constraints.gridwidth = 2; //two columns wide
-        this.add(lblTitle, constraints); // adds the component
 
-        constraints.gridy = 1;
+        constraints.gridy = 0;
         constraints.gridx = 0;          //New row
         constraints.gridwidth = 1;
         this.add(lblStormName, constraints);
 
         constraints.gridx = 1;
+        constraints.gridy = 0;          //New row
         //makes text box fill width of column
-        constraints.fill = GridBagConstraints.HORIZONTAL;
+//        constraints.fill = GridBagConstraints.HORIZONTAL;
         this.add(txtStormName, constraints);
-        
-        constraints.fill = GridBagConstraints.NONE;
+
+        //      constraints.fill = GridBagConstraints.NONE;
         constraints.gridy = 2;
         constraints.gridx = 0;
         this.add(lblWindSpeed, constraints);
-        
-        constraints.fill = GridBagConstraints.HORIZONTAL;
+
+        //    constraints.fill = GridBagConstraints.HORIZONTAL;
         constraints.gridx = 1;
         this.add(txtWindSpeed, constraints);
-        
-        constraints.fill = GridBagConstraints.NONE;
+
+        //    constraints.fill = GridBagConstraints.NONE;
         constraints.gridy = 3;
         constraints.gridx = 0;
-        this.add(lblTemperature);
-        
-        constraints.fill = GridBagConstraints.HORIZONTAL;
+        this.add(lblTemperature, constraints);
+
+        //    constraints.fill = GridBagConstraints.HORIZONTAL;
         constraints.gridx = 1;
         this.add(txtTemperature, constraints);
-        
-        constraints.fill = GridBagConstraints.NONE;
+
+
+        constraints.gridy = 5;
+        constraints.gridx = 1;
+        this.add(btnSave, constraints);
+
+                //    constraints.fill = GridBagConstraints.NONE;
+        constraints.gridy = 6;
+        constraints.gridx = 1;
+        this.add(btnDelete, constraints);
+
         constraints.gridy = 4;
         constraints.gridx = 0;
-        this.add(btnEdit, constraints);    
-        
-    }
+        constraints.gridwidth = 2;
+        this.add(lblWarning, constraints);
 
+    }
 
     public void setStorm(Storm storm) {
         // display contents of this storm
@@ -128,5 +155,6 @@ public class EditStorm extends JPanel {
         txtStormName.setText(storm.getName());
         txtWindSpeed.setText("" + storm.getWindSpeed());
         txtTemperature.setText("" + storm.getTemp());
+        lblWarning.setText("<html>" + storm.getWarning() + "</html>");
     }
 }
