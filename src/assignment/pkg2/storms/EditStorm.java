ca0b68b9e5442e8ae622a23586c874b841005347
charlie249 <56076410+charlie249@users.noreply.github.com>
2020-01-09 11:07:09 +0000

    Making the buttons, textboxes and labels for Edit()

--- src/assignment/pkg2/storms/EditStorm.java
+++ src/assignment/pkg2/storms/EditStorm.java
@@ -6,15 +6,42 @@
 package assignment.pkg2.storms;
 
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JFrame;
+import javax.swing.JButton;
 
 /**
  *
  * @author ian
  */
-public class EditStorm extends JPanel {
+public class EditStorm extends JFrame {
+        public JLabel lblTitle;
+        public JLabel lblStormName;
+        public JLabel lblWindSpeed;
+        public JLabel lblTemperature;
+        
+        public JTextField txtStormName;
+        public JTextField txtWindSpeed;
+        public JTextField txtTemperature;
+        
+        public JButton btnEdit;
+        
     public EditStorm()
     {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
+        lblTitle = new JLabel("Edit Storm");
+        
+        lblStormName = new JLabel("Storm Name: ");
+        lblWindSpeed = new JLabel("WindSpeed: ");
+        lblTemperature = new JLabel("Temperature: ");
+        
+        txtStormName = new JTextField();
+        txtWindSpeed = new JTextField();
+        txtTemperature = new JTextField();
+        
+        btnEdit = new JButton("Save new storm data");
     }
     
 }
